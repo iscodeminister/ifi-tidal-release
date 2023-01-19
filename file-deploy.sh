7c80b83d91171099656516c6e25c799cc4449acb
iscodeminister <50560278+iscodeminister@users.noreply.github.com>
2023-01-19 23:56:28 +0800

    Update file-deploy.sh

--- file-deploy.sh
+++ file-deploy.sh
@@ -2,4 +2,4 @@
 cp /usr/ifi/ifi-tidal-release/ifi-streamer-tidal-connect.service /lib/systemd/system/
 chmod +x /usr/ifi/ifi-tidal-release/play
 chmod +x /usr/ifi/ifi-tidal-release/pa_devs/run.sh
-systemctl enable ifi-streamer-tidal-connect.service
+#systemctl enable ifi-streamer-tidal-connect.service
