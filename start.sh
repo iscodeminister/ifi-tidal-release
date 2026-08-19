fb761042948bc6e8ed927a939821996099513f75
iscodeminister <a45281687@hotmail.com>
2026-08-20 01:51:32 +0800

    Add Makefile, select-device.sh, and start.sh scripts; include service management for tidal-connect

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$PWD/lib
+DEVICE="USB Audio:"
+./bin/tidal_connect_application \
+	--tc-certificate-path "./id_certificate/tcon.crt" \
+	--netif-for-deviceid eth0 \
+	-f "Connect Streamer" \
+	--codec-mpegh true \
+	--codec-mqa false \
+	--model-name "iFi Streamer" \
+	--disable-app-security false \
+	--disable-web-security false \
+	--enable-mqa-passthrough false \
+	--playback-device "$DEVICE" \
+    --clientid "VCjoaRrbaMU005Tk" 
+	--log-level 1 
